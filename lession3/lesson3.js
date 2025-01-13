81de4ace54490fb8eb42e11df834c3bd8f4e3d42
Igor <66474874+Iazovski@users.noreply.github.com>
2025-01-13 16:32:23 +0800

    Delete lession3 directory

--- lession3/lesson3.js
+++
@@ -1,6 +0,0 @@
-let titleProject = 'Оценка стоимости';
-let screensValue = 'Шаблонные, с уникальным дизайном, с анимациями';
-let screenPrice = 300;
-let percentage = 5;
-let fullPrice = 1500;
-let responsive = true;
