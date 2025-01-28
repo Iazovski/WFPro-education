2dc2cd9f7af7c676788f18e10f3817963c1d5e27
Iazovski <igor.yazovskiy@gmail.com>
2025-01-28 06:42:07 +0100

    next version

--- lesson6/lesson6.js
+++ lesson6/lesson6.js
@@ -1,38 +1,10 @@
-let screenPrice = 30000;
+let screenPrice;
 let percentage = 10;
-
-let titleProject = prompt('Укажите название проекта');
-console.log(titleProject);
-
-let screensValue = prompt('Укажите типы экранов');
-console.log(screensValue);
-
-let responsive = prompt('Нужен ли адаптивный сайт?');
-console.log(responsive);
-
-let service = prompt('Какой сервис нужен?');
-console.log(service);
-
-let servicePrice = prompt('Сколько это будет стоить?');
-console.log(servicePrice);
-
-let serviceYet = prompt('Какой еще сервис тебе нужен?');
-console.log(serviceYet);
-
-let serviceYetPrice = prompt('Сколько будет стоить этот второй сервис?');
-console.log(serviceYetPrice);
-
-let getAllServicePrices = function (servicePrice,serviceYetPrice) {
-    return +servicePrice + +serviceYetPrice;
-};
-const allServicePrices = getAllServicePrices(servicePrice,serviceYetPrice);
-console.log(allServicePrices);
-
-function getFullPrice () {
-    return allServicePrices + screenPrice;
-};
-const fullPriceServices = getFullPrice();
-console.log(fullPriceServices);
+let titleProject;
+let screensValue;
+let responsive;
+let service;
+let serviceYet;
 
 function getTitle () {
     let firstChar = titleProject.charAt(0);
@@ -40,50 +12,85 @@ function getTitle () {
     let restOfString = titleProject.slice(1);  
     let restOfStringLow = restOfString.toLowerCase();
     let newTitleProject = upperFirstChar + restOfStringLow;
-    console.log(newTitleProject.replaceAll(' ', ''));
 };
 
-function getServicePercentPrices () {
-    let servicePercentPrice = Math.round(fullPriceServices - fullPriceServices * percentage / 100);
-    return fullPriceServices - servicePercentPrice;
+let checkIsNumber = (num) => {
+    return !isNaN(parseFloat(num)) && isFinite(num);
 };
 
-let servicePercentPriceWithout = getServicePercentPrices();
-console.log(servicePercentPriceWithout);
+const briefing = () => {
+    titleProject = prompt('Укажите название проекта');
+    screensValue = prompt('Укажите типы экранов');
+    screenPrice = prompt('Сколько будет стоить экран?');
 
-function getRollbackMessage() {
-    if (fullPriceServices >= 50000) {
-        console.log('Даем скидку 10%');
-    
-    } else if (fullPriceServices >= 20000 && fullPriceServices < 50000) {
-        console.log('Даем скидку 5%');
-    
-    }  else if (20000 > fullPriceServices > 0) {
-        console.log('Скидка не предусмотрена');
-    
-    } else if (fullPriceServices < 0) {
-        console.log('Что-то пошло не так');
+    while (!checkIsNumber(screenPrice) || screenPrice === NaN || screenPrice.trim() === '') {
+        screenPrice = prompt('Сколько будет стоить экран?');
+    }; 
+    screenPrice = Number(screenPrice);
+    responsive = prompt('Нужен ли адаптивный сайт?');
+};
+
+//общая сумма всех услуг
+const getAllServicePrices = () => {
+    let sumService = 0;
+
+    for (i = 0; i < 2; i++) {
+
+        if (i === 0){
+            service = prompt('Какой сервис нужен?');
+        } else if (i === 1) {
+            serviceYet = prompt('Какой еще сервис тебе нужен?');
+        };
+
+        let promtPrice= '';
+
+        while (!checkIsNumber(promtPrice) || promtPrice === null || promtPrice.trim() === '') {
+            promtPrice = prompt('Сколько это будет стоить?');
+        }; 
+
+        sumService = sumService + Number(promtPrice);
     };
 };
 
-let checkIsNumber = () => {
-    function isNan(num) {
-        if (parceFloat(num) && isFinite(num)) {
-            console.log('это число') 
-            };
-        };
-    }
 
 
-const screenPriceNum = checkIsNumber () ;
 
-let submit = confirm ('Подтвердите переход на страницу');
-console.log(submit);
+function getFullPrice () {
+    return screenPrice + allServicePrices;
+}
 
-while (screenPriceNum !== NaN || submit == true || screenPriceNum.trim()) {
-    screenPrice = prompt('Сколько будет стоить экран?');
+function getServicePercentPrices () {
+    return fullPrice - (fullPrice * (percentage / 100));
 };
 
-getTitle();
-getServicePercentPrices();
-getRollbackMessage();
+
+
+let getDiscount = (value) => {
+    if (value >= 50000) {
+        return 'Даем скидку 10%';
+    } else if (value >= 20000 && value < 50000) {
+        return 'Даем скидку 5%';
+    }  else if (20000 > value > 0) {
+        return 'Скидка не предусмотрена';
+    } else if (value < 0) {
+        return 'Что-то пошло не так';
+    };
+};
+
+
+let submit = confirm ('Подтвердите переход на страницу');
+
+briefing();
+let allServicePrices = getAllServicePrices();
+let fullPrice = getFullPrice();
+let servicePercentPrice = getServicePercentPrices();
+let newTitleProject = getTitle();
+
+
+console.log(newTitleProject);
+console.log(allServicePrices);
+console.log(fullPrice);
+console.log(Math.ceil(servicePercentPrice));
+console.log(serviceYet);
+console.log(getServicePercentPrices(fullPrice));
+
